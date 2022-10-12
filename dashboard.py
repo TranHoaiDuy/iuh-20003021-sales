ff2ea8b20b655cc917ec26ae54fde753ebfdcdf9
Tuanper <90364173+Tuanper@users.noreply.github.com>
2022-10-12 12:21:53 +0700

    new

--- dashboard.py
+++ dashboard.py
@@ -6,12 +6,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import dash
 
-cred = credentials.Certificate('./iuh-20079191-firebase-adminsdk-44xct-7031340b3a.json')
+cred = credentials.Certificate('./iuh-20009931-nguyenhoangthai-firebase-adminsdk-ucgmk-91c6254c8e.json')
 appLoadData = firebase_admin.initialize_app(cred)
 
 dbFireStore = firestore.client()
 
-queryResults = list(dbFireStore.collection(u'tbl20079191').stream())
+queryResults = list(dbFireStore.collection(u'tbl20009931').stream())
 listQueryResult = list(map(lambda x: x.to_dict(), queryResults))
 
 df = pd.DataFrame(listQueryResult)
